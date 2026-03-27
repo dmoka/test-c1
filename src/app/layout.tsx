83506c2242a6b0cd42aaf4edce177d70d81aac73
dmoka <mokabme@gmail.com>
2026-03-27 19:54:14 +0100

    chore: change title to Hello Peti calculator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "A superior calculator",
+  title: "Hello Peti calculator",
   description: "Simple, clean web calculator for basic arithmetic operations",
 };
 
