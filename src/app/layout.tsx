71fe7f92324a341dd8aef974862ee1a9defc75d6
dmoka <mokabme@gmail.com>
2026-03-28 11:59:10 +0100

    cím módosítás: Best Niki Super

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Best Dani Super",
+  title: "Best Niki Super",
   description: "Simple, clean web calculator for basic arithmetic operations",
 };
 
