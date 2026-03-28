93e32cc0f441d7f89e7ca9733231f5bcaa76ea55
dmoka <mokabme@gmail.com>
2026-03-28 11:56:14 +0100

    cím módosítás: Best C Super

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Best Calc",
+  title: "Best C Super",
   description: "Simple, clean web calculator for basic arithmetic operations",
 };
 
