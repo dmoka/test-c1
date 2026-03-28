f71521f3fcaf16692ba64eedf23daf5a86db81a4
dmoka <mokabme@gmail.com>
2026-03-28 15:56:01 +0100

    cím módosítás: Dani's best calculator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Legjobb Calculator",
+  title: "Dani's best calculator",
   description: "Simple, clean web calculator for basic arithmetic operations",
 };
 
